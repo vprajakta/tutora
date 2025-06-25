a62ec89eca1b42f9dfd2c9f68c8d6170a4033289
Prajakta <varuteprajakta1@gmail.com>
2025-06-25 16:49:26 +0530

    fronend url changed

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ await connectCloudinary()
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tutora-ecru.vercel.app"],
+    origin: ["http://localhost:5173", "https://tutora-rho.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -51,7 +51,7 @@ const server = createServer(app);
 //Initialize IO
 // const io = new Server(server, {
 //   cors: {
-//     origin: "https://tutora-ecru.vercel.app",
+//     origin: "tutora-rho.vercel.app",
 //     methods: ["GET", "POST", "PUT", "DELETE"],
 //     credentials: true,
 //   },
@@ -59,7 +59,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://tutora-ecru.vercel.app"],
+    origin: ["http://localhost:5173", "https://tutora-rho.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
