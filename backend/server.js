8a30a4a2e5a747059e4d2f482f8b95f57f81c81e
Prajakta <varuteprajakta1@gmail.com>
2025-06-25 16:34:04 +0530

    server changed

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ await connectCloudinary()
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tutora-ecru.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -59,7 +59,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tutora-ecru.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
